c1b001781aaa6ecc40fee7775bdbcc33c66f7b65
Ramji-44 <ramjisanmugam@gmail.com>
2026-05-14 11:21:15 +0530

    updated dashboard page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import "./App.css"
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
 import Form from "./components/Form/Form"
+import Tasks from "./components/Tasks/Tasks"
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Form/>
+      <div className="title">
+        <span className="line"></span>
+        <h1>Task Dashboard</h1>
+      </div>
+
+      <main className="main-container">
+        <div className="form-section">
+          <Form />
+        </div>
+
+        <div className="tasks-section">
+          <Tasks />
+        </div>
+      </main>
+
       <Footer />
     </>
   )
---
+++ src/components/Tasks/Tasks.jsx
@@ -0,0 +1,8 @@
+function Tasks(){
+    return(
+        <div className="tasks-page">
+        <h1>Tasks page</h1>
+        </div>
+    )
+}
+export default Tasks
