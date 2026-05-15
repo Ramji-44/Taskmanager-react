d42654d5a17b137558b826729108b50b4988a79f
Ramji-44 <ramjisanmugam@gmail.com>
2026-05-15 22:54:14 +0530

    added tick,xmark etc icons

--- src/utils/Fontawesome.js
+++ src/utils/Fontawesome.js
@@ -1,10 +1,12 @@
+import { faHeart, faTrash, faCircleExclamation, faCheck, faXmark, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
-import { faHeart, faTrash, faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
-
- const icons = {
+const icons = {
   heart: faHeart,
   trash: faTrash,
   warning: faCircleExclamation,
+  tick: faCheck,
+  xmark: faXmark,
+  edit: faPenToSquare
 }
 
 export default icons
