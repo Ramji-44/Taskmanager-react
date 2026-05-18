470e86f43ce5743c075206f96d69c2e3210ef120
Ramji-44 <ramjisanmugam@gmail.com>
2026-05-18 13:14:22 +0530

    added a smaple data for UI styling and set a height for tasks component

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -31,7 +31,7 @@ function Tasks({ tasks = [] }) {
         {
             id: 3,
             title: "Ramji Sanmugam",
-            description: "You've identified a common React performance issue! The Form re-renders on every input change, which causes all CustomInput components to re-render as well—even though they're memoized. Here are several solutions:",
+            description: "may god bless you",
             dueDate: "Mar 20, 2003",
             assignedTo: "Quinn Rios",
             priority: "high",
@@ -40,7 +40,62 @@ function Tasks({ tasks = [] }) {
         {
             id: 4,
             title: "john wick",
-            description: "which causes all CustomInput components to re-render as well—even though they're memoized. Here are several solutions:",
+            description: "Models are the essence of Sequelize. A model is an abstraction that represents a table in your database. In Sequelize, it is a class that extends Model.",
+            dueDate: "Mar 20, 2003",
+            assignedTo: "Quinn Rios",
+            priority: "medium",
+            status: "Completed"
+        },
+        {
+            id: 5,
+            title: "Megan Sweet4",
+            description: "fdsfgfgfgh",
+            dueDate: "Jun 6, 2026",
+            assignedTo: "Sydney Mann",
+            priority: "medium",
+            status: "In Progress"
+        },
+
+        {
+            id: 6,
+            title: "Jelani Banks",
+            description: "Cillum illo omnis co",
+            dueDate: "Aug 31, 2026",
+            assignedTo: "Quinn Rios",
+            priority: "low",
+            status: "Pending"
+        },
+        {
+            id: 7,
+            title: "Ramji Sanmugam",
+            description: "A watch is a timepiece carried or worn by a person. It is designed to maintain a consistent movement despite the motions caused by the person's activities",
+            dueDate: "Mar 20, 2003",
+            assignedTo: "Quinn Rios",
+            priority: "high",
+            status: "Completed"
+        },
+        {
+            id: 8,
+            title: "john wick",
+            description: "The model tells Sequelize several things about the entity it represents, such as the name of the table in the database and which columns it has (and their data types).",
+            dueDate: "Mar 20, 2003",
+            assignedTo: "Quinn Rios",
+            priority: "medium",
+            status: "Completed"
+        },
+        {
+            id: 9,
+            title: "john wick",
+            description: "Modern watches often display the day, date month, and year. Mechanical watches may have extra features such as moon-phase displays and different types of tourbillon.",
+            dueDate: "Mar 20, 2003",
+            assignedTo: "Quinn Rios",
+            priority: "medium",
+            status: "Completed"
+        },
+        {
+            id: 10,
+            title: "john wick",
+            description: "A pocket watch is carried in a pocket, often attached to a chain. A stopwatch is a type of watch that measures intervals of time.",
             dueDate: "Mar 20, 2003",
             assignedTo: "Quinn Rios",
             priority: "medium",
@@ -82,16 +137,7 @@ function Tasks({ tasks = [] }) {
             </div>
 
             <div className={styles.taskCards}>
-
-                {filterTasks.map(task => (
-
-                    <TaskCard
-                        key={task.id}
-                        task={task}
-                    />
-
-                ))}
-
+                {filterTasks.map(task => (<TaskCard key={task.id} task={task} />))}
             </div>
 
         </section>
