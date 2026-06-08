71dd3f41f4e57cc16f38f62ab1ea6814536319a8
Ramji-44 <ramjisanmugam@gmail.com>
2026-06-08 14:43:09 +0530

    import and use router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/dashboard" />
+  },
+  {
+    path: "/dashboard",
+    element: <App />
+  }
+])
+
 createRoot(document.getElementById('root')).render(
-    <App />
+    < RouterProvider router={router} />
 )
