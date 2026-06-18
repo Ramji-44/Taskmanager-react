ce363fde21f127d97de2692cbf4e98189e6318d7
Ramji-44 <ramjisanmugam@gmail.com>
2026-06-18 10:48:36 +0530

    add config setups

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    port : 3000
+  server: {
+    port: 3000
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",  // fake browser
+    setupFiles: "./src/tests/setup.js"  // global file for import
   }
 })
